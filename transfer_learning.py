05d96739078c3bf041ab362479b8a54aa0d4aef4
arshinmar <arsh.kadakia18@gmail.com>
2020-11-27 15:05:12 -0500

    updated with better file name

--- transfer_learning.py
+++ transfer_learning.py
@@ -119,10 +119,10 @@ if __name__ == '__main__':
     GLOBALS.CONFIG = initialize_hyper('config.yaml')
 
     try:
-        os.mkdir(os.path.join(output_path,'LR={}_batchsize={}_epochs={}'.format(GLOBALS.CONFIG['LR'],GLOBALS.CONFIG['batch_size'],GLOBALS.CONFIG['epochs'])))
+        os.mkdir(os.path.join(output_path,'Model={}_LR={}_batchsize={}_epochs={}'.format(GLOBALS.CONFIG['model_name'],GLOBALS.CONFIG['LR'],GLOBALS.CONFIG['batch_size'],GLOBALS.CONFIG['epochs'])))
     except:
         pass
-    data_folder_path = os.path.join(output_path,'LR={}_batchsize={}_epochs={}'.format(GLOBALS.CONFIG['LR'],GLOBALS.CONFIG['batch_size'],GLOBALS.CONFIG['epochs']))
+    data_folder_path = os.path.join(output_path,'Model={}_LR={}_batchsize={}_epochs={}'.format(GLOBALS.CONFIG['model_name'],GLOBALS.CONFIG['LR'],GLOBALS.CONFIG['batch_size'],GLOBALS.CONFIG['epochs']))
 
 
 
