9a0b7d176f73f891ff6c605eeff25be7e007b683
arshinmar <arsh.kadakia18@gmail.com>
2020-11-26 21:04:03 -0500

    Fixed syntax error

--- dataloaders.py
+++ dataloaders.py
@@ -74,7 +74,7 @@ def dataloaders(main_file_directory,batch_size = 1):
     # Normalize with calculated mean and std (approximate calculated on subset of data)
     train_transforms = transforms.Compose(train_transforms + [transforms.Normalize(train_mean,train_std)])
     val_transforms = transforms.Compose(val_transforms + [transforms.Normalize(val_mean,val_std)])
-    test_transforms = transforms.Compose(test_transforms + [transforms.Normalize(test_mean,test_std])])
+    test_transforms = transforms.Compose(test_transforms + [transforms.Normalize(test_mean,test_std)])
 
     transform_list = [train_transforms, val_transforms, test_transforms]
 
